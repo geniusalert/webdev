a54adf8f9a3159c78b9994d68c2c810f09b0014b
Govind Tiwari <govind.tiwari2709@gmail.com>
2024-08-08 19:57:11 +0530

    Update day3.js

---
+++ day3.js
@@ -0,0 +1,23 @@
+function topi(n) {
+    let ans = 0 ;
+    for (let i = 1 ; i <=n ; i++) {
+      ans = ans + i ;
+    }
+      return ans;
+    }
+    
+    let haha = topi(50)
+    console.log(haha);
+    
+    function sqr(n) {
+      return n*n;
+    }
+    
+    function sumofsquare(a, b,  ) {
+    
+      return = a + b + sqr(c)
+    }
+    
+    console.log(sumofsquare(2, 3, sqr (4));
+
+    
